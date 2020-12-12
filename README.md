17dbabb9a488c26f205f68219c74ba8c924496d7
sydneywalcoff <75542938+sydneywalcoff@users.noreply.github.com>
2020-12-12 12:59:13 -0800

    re-done project tohave three separate objects

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # inspirational-message-generator
+A codecademy portfolio project to practice generating a 3 part inspirational message.
--- generator.js
+++ generator.js
@@ -1,18 +1,36 @@
-const adj = ['smart', 'kind', 'important', 'beautiful', 'intelligent', 'powerful', 'empowered', 'compassionate', 'nice', 'articulate', 'hard-working', 'innovative', 'lovely']
-const generator = arr => {
-    let adj1 = adj[Math.floor(Math.random() * adj.length)];
-    let adj2 = adj[Math.floor(Math.random() * adj.length)];
-    let adj3 = adj[Math.floor(Math.random() * adj.length)];
-    if (adj2 === adj1 || adj2 === adj3) {
-        let randIndex = Math.floor(Math.random() * adj.length);
-        adj2 = adj[randIndex];
-    } else if (adj3 === adj1 || adj3 === adj2) {
-        let randIndex = Math.floor(Math.random() * adj.length);
-        adj3 = adj[randIndex];
-    };
-    return `You are ${adj1}, ${adj2}, and ${adj3}. Have a good day <3!`
+const randNumGen = num => {
+    return Math.floor(Math.random() * num);
 };
 
-console.log(generator(adj));
+const wisdom = {
+    badThing: ['empty', 'dark', 'sad', 'broken', 'a dumpster fire', 'stressful', 'a nightmare'],
+    adj: ['smart', 'kind', 'important', 'beautiful', 'intelligent', 'powerful', 'empowered', 'compassionate', 'nice', 'incredible', 'hard-working', 'wonderful', 'lovely'],
+    encouragement: ['Keep hanging on', 'You can do it', 'Everything will be okay', 'You are stronger than this', 'Take a breath, everything will be fine']
+};
 
+const advice = [];
 
+for(let prop in wisdom) {
+    let randIndex = randNumGen(wisdom[prop].length);
+
+    switch (prop) {
+        case 'badThing':
+            advice.push(`The world is ${wisdom[prop][randIndex]}, but`);
+            break;
+        case 'adj':
+            advice.push(`you are ${wisdom[prop][randIndex]}!`);
+            break;
+        case 'encouragement':
+            advice.push(`${wisdom[prop][randIndex]}!`);
+            break;
+        default:
+            return 'not enough info';
+    }
+}
+
+const formatWisdom = () => {
+    const formatted = advice.join(' ');
+    console.log(formatted);
+};
+
+formatWisdom(wisdom);
